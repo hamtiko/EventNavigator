6ff66f625b6b65b5343db5a4ea24fca66055fa95
Tigran Hambardzumyan <tigran.hambardzumyan@picsart.com>
2024-09-18 01:02:30 +0400

    Initial Commit

---
+++ EventNavigator/EventNavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventNavigatorApp.swift
+//  EventNavigator
+//
+//  Created by Tigran Hambardzumyan on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventNavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
